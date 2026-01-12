f03f5cb7a021c7e81d8e4de0c1764db1e8a10547
Anastasia <anas.afonina@mail.ru>
2026-01-12 15:48:24 +0300

    fix: link Telegram to contact button

--- src/components/SocLinks/index.tsx
+++ src/components/SocLinks/index.tsx
@@ -14,24 +14,27 @@ type SocLinksProps = {
 
 const SocLinks = ({ mode = 'dark' }: SocLinksProps) => {
   const { isDesktop } = useDevice();
-
+  const ALLOWED_SOCIALS: IconName[] = ['behance', 'dprofile', 'dribbble'];
+  const filteredSocLinks = socLinks.filter(link =>
+    ALLOWED_SOCIALS.includes(link.name.toLowerCase() as IconName),
+  );
   return (
     <div className='flex items-center gap-2.5 text-white/30 max-md:-order-1'>
       {isDesktop
-        ? socLinks.map((link, index) => (
-            <AnimatedLink key={link.name} showDot={index > 0 && index < socLinks.length} name={link.name} />
-          ))
-        : socLinks.map((link) => (
-            <Button
-              key={link.name}
-              link={link.url}
-              className={cn('text-[#F7F7F7]', {
-                'border-[#00000029] text-black hover:border-[#00000029]': mode === 'light',
-              })}
-              iconLeft={link.name.toLowerCase() as IconName}
-              animation={true}
-            />
-          ))}
+        ? filteredSocLinks.map((link, index) => (
+          <AnimatedLink key={link.name} showDot={index > 0 && index < filteredSocLinks.length} href={link.url} name={link.name} />
+        ))
+        : filteredSocLinks.map((link) => (
+          <Button
+            key={link.name}
+            link={link.url}
+            className={cn('text-[#F7F7F7]', {
+              'border-[#00000029] text-black hover:border-[#00000029]': mode === 'light',
+            })}
+            iconLeft={link.name.toLowerCase() as IconName}
+            animation={true}
+          />
+        ))}
     </div>
   );
 };
